629173c8c288389e632c808d3f4e4f3b98ac8331
Pawitra Warda <pawitrawarda@gmail.com>
2020-03-17 11:07:53 +0700

    add bootstrap and base UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Container, Row, Col, Tab, Nav, Button } from 'react-bootstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <Container>
+            <section className="posts-box">
+                <Row>
+                    <Col md={8} className="mx-auto">
+                        <Row>
+                            <Col md={12}>
+                                <textarea className="col-12" placeholder="type your post.."></textarea>
+                            </Col>
+                            <Col md={12} className="text-right">
+                                <Button variant="success">Post</Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </section>
+            <section className="contents">
+                <Tab.Container defaultActiveKey="mypost">
+                    <Row>
+                        <Col md={12}>
+                            <Nav variant="pills" justify>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="mypost">My Posts</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="users">Users</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="myalbum">My Album</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="mypost">
+                                    First
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="users">
+                                    Second
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="myalbum">
+                                    Second
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
+            </section>
+        </Container>
+    );
+  }
 }
 
 export default App;
